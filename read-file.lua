a80d36a38f5194821127b81361100ac0c47aee18
CogentRedTester <cogent.redtester@outlook.com>
2021-10-03 18:34:18 +1030

    allow getters to return either string or file handle
    The get_file function will now convert the types between eachother
    when needed, depending on what the user requests

--- read-file.lua
+++ read-file.lua
@@ -4,6 +4,7 @@
 ]]--
 
 local mp = require "mp"
+local msg = require "mp.msg"
 local utils = require "mp.utils"
 local opts = require "mp.options"
 
@@ -14,7 +15,10 @@ local o = {
 opts.read_options(o, "read_file")
 
 local rf = {}
+local temp_files = {}
 
+--this platform test was taken from mpv's console.lua
+local PLATFORM_WINDOWS = mp.get_property_native('options/vo-mmcss-profile', o) ~= o
 
 local function execute(args)
     local cmd = mp.command_native({
@@ -35,6 +39,24 @@ local function execute(args)
     end
 end
 
+--gets the path of a temporary file that can be used by the script
+local function get_temp_file_name()
+    local file = os.tmpname()
+    if not PLATFORM_WINDOWS then return file
+    else return mp.command_native({"expand-path", "~/AppData/Local/Temp"}) .. file end
+end
+
+--creates a new temporary file with the given contents, and returns a file read handle for this file
+local function get_temp_file_handler(contents)
+    local filename = get_temp_file_name()
+    table.insert(temp_files, filename)
+
+    local tmpfile = io.open(filename, "w")
+    tmpfile:write(contents)
+    tmpfile:close()
+
+    return io.open(filename, "r")
+end
 local function get_protocol(uri)
     return uri:match("^(%a%w*)://")
 end
@@ -44,7 +66,7 @@ local function get_local(file)
 end
 
 local function get_wget(file)
-    return io.popen( ("wget -q -O - %q %s"):format(file, o.wget_flags), "r" )
+    return execute({"wget", "-O", "-", file, o.wget_flags})
 end
 
 --tracks what functions should be used for specific protocols
@@ -54,30 +76,46 @@ local protocols = {
     https = get_wget,
 }
 
---returns a file handler for the given file
-function rf.get_file_handler(file)
+--uses the protocol of the file uri to determine what get function to run, and converts the result into
+--either a string or a file handle, depending on the second argument. If as_string is nil then return whichever
+--type the get function defaults to
+local function get_file(file, as_string)
     local path = file
     local protocol = get_protocol(file)
-    local get_handler = nil
+    local get_method = nil
+
+    --determines what utility to use to read the file
     if not protocol then
         path = utils.join_path(mp.get_property("working-directory", ""), file)
-        get_handler = get_local
+        get_method = get_local
     else
-        get_handler = protocols[protocol] or get_wget
+        get_method = protocols[protocol] or get_wget
     end
 
-    return get_handler(path)
+    local contents = get_method(path)
+    if not contents or as_string == nil then return nil end
+
+    --converts the result of the get function into the correct output type - either a string or a file handle
+    if as_string and io.type(contents) then
+        local tmp = contents
+        contents = tmp:read("*a")
+        tmp:close()
+    elseif not as_string and not io.type(contents) then
+        contents = get_temp_file_handler(contents)
+    end
+
+    return contents
+end
+
+--returns a file handler for the given file
+function rf.get_file_handler(file)
+    return get_file(file, false)
 end
 
 --reads the contents of the file to a string and returns the result
 --if the file could not be read then return nil
 function rf.read_file(file)
-    local handler = rf.get_file_handler(file)
-    if not handler then return nil end
-
-    local contents = handler:read("*a")
-    handler:close()
-    return contents
+    return get_file(file, true)
 end
 
 --returns an iterator for the lines in the file, which closes the file once EOF is reached
